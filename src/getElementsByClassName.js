154025b95a6820db15939063c3b0c6e601cc1832
singer30 <lukas.w.singer@gmail.com>
2019-01-02 22:00:23 -0800

    wrote and implementation for getElementsByClassName but currently fails do to contains.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,32 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
   // your code here
+  var objArray = [];
+  
+  
+  
+  if(arguments.length === 1){
+    var node = document.body;
+    if (node.classList.contains(className)){
+      objArray.push(node)
+    }
+    var children = node.childNodes
+    if (children.length > 0){
+      for(var i = 0; i < children.length; i++){
+        objArray.concat(getElementsByClassName(className, children[i]))
+      }
+    }
+  } else {
+    var node = arguments[1];
+    if (node.classList.contains(className)){
+      objArray.push(node)
+    }
+    var children = node.childNodes
+    if (children.length > 0){
+      for(var i = 0; i < children.length; i++){
+        objArray.concat(getElementsByClassName(className, children[i]))
+      }
+    }
+  }
+  return objArray;
 };
