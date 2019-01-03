05b51ec49271d0abb93fa410a328d095bfa74de5
singer30 <lukas.w.singer@gmail.com>
2019-01-02 16:26:10 -0800

    need to fix double spacing in array in stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,6 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  if(typeof obj === 'number' || typeof obj === 'boolean'){
+    return String(obj);
+  }
+  if ( obj === null || typeof obj === 'undefined'){
+    return "null"; 
+  }
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  if(Array.isArray(obj)){
+    var start = '[';
+    if (obj.length === 0) {
+      return start += ']';
+    }
+    for (var i = 0; i < obj.length; i++) {
+      if (i === obj.length - 1){
+        if (typeof obj[i] === 'string') {
+          start += '"' + obj[i] + '"';
+        } else {
+          start += obj[i];
+        }    
+      } else {
+        if (typeof obj[i] === 'string') {
+          start += '"' + obj[i] + '"' + ',';
+        } else {
+          start += obj[i] + ',';
+        }
+      }
+    }
+  }
+  return start += ']';     
+};
+
+/*
+input: element
+output: element with added quotes
+if typeof obj === 'string'
+return " + obj
+else return back into strin
+
+
+
+
 var targetKeys = [];
 var targetValue = [];
   if (Array.isArray(obj)){
@@ -18,15 +61,4 @@ var targetValue = [];
     }
      return obj
   }
-};
-
-/*
-input: element
-output: element with added quotes
-if typeof obj === 'string'
-return " + obj
-else return back into strin
-
 */ 
-
-
